26520503eb1c7e1343e9b9896b7af80054af3484
zhyu <you@example.com>
2021-09-08 07:53:30 +0800

    ok

--- src/main/java/person/pluto/natcross2/CommonConstants.java
+++ src/main/java/person/pluto/natcross2/CommonConstants.java
@@ -11,15 +11,28 @@ package person.pluto.natcross2;
 public final class CommonConstants {
 
 	// 服务端地址，支持IP或域名，这个根据服务端放的网络位置进行设置
-	public static final String serviceIp = "127.0.0.1";
+	public static final String serviceIp = "120.79.186.158";
 	// 客户端服务的端口
 	public static final int servicePort = 10010;
 
 	// 映射对
 	public static ListenDest[] listenDestArray = new ListenDest[] {
-			//
-			ListenDest.of(8081, "127.0.0.1", 8080),
-			//
+			//zk
+			ListenDest.of(2181, "127.0.0.1", 2181),
+			ListenDest.of(2888, "127.0.0.1", 2888),
+			ListenDest.of(3888, "127.0.0.1", 3888),
+			//zkui
+			ListenDest.of(9090, "127.0.0.1", 9090),
+			//mysql
+			ListenDest.of(3306, "127.0.0.1", 3306),
+			//redis
+			ListenDest.of(6379, "127.0.0.1", 6379),
+			//gateway
+			ListenDest.of(7000, "127.0.0.1", 7000),
+			//auth
+			ListenDest.of(7001, "127.0.0.1", 7001),
+			//blog
+			ListenDest.of(7002, "127.0.0.1", 7002),
 	};
 
 	// 交互密钥 AES
